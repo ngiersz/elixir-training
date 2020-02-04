1e8902ad3211c0c6f18b486804c6d7f594bf0cce
ngiersz <ngiersz@gmail.com>
2020-02-04 19:09:55 +0100

    Use codepoint syntax and change function to private

--- exercism/elixir/rna-transcription/lib/rna_transcription.ex
+++ exercism/elixir/rna-transcription/lib/rna_transcription.ex
@@ -10,18 +10,15 @@ defmodule RnaTranscription do
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
     dna
-    |> Enum.chunk_every(1)
     |> Enum.map(&get_rna_nucleotide/1)
-    |> Enum.join
-    |> to_charlist
   end
 
-  def get_rna_nucleotide(dna_nucleotide) do
-    case(to_string(dna_nucleotide)) do
-      "G" -> 'C'
-      "C" -> 'G'
-      "T" -> 'A'
-      "A" -> 'U'
+  defp get_rna_nucleotide(dna_nucleotide) do
+    case(dna_nucleotide) do
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
     end
   end
 
